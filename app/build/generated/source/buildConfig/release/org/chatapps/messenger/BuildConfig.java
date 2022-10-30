5583edb2f6ffa2f051c9e3ca32879871d7158234
kakatua01 <39296186+kakatua01@users.noreply.github.com>
2022-10-30 10:38:39 +0700

    upload baru
    upload

---
+++ app/build/generated/source/buildConfig/release/org/chatapps/messenger/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.chatapps.messenger;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.chatapps.messenger";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.1.0";
+  // Field from default config.
+  public static final String BUILD_TIMESTAMP = "20221008173204";
+  // Field from default config.
+  public static final String BUILD_VERSION = "1.1.0";
+}
